25c50e5f130b96d56b17b418e396ddaa35bd45cf
abhijeet <redefinedabhi2@gmail.com>
2022-07-02 03:31:55 +0530

    Created Playlist & Favorite Music Activity

---
+++ app/src/main/java/com/theleafapps/pro/audisc/PlaylistActivity.kt
@@ -0,0 +1,11 @@
+package com.theleafapps.pro.audisc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaylistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist)
+    }
+}
