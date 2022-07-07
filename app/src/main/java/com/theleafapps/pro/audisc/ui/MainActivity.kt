8e8e3e3a0ad748e39e6a47527deb89afc671471d
abhijeet <redefinedabhi2@gmail.com>
2022-07-07 11:27:29 +0530

    Implementing Navigation Menu in MainActivity

--- app/src/main/java/com/theleafapps/pro/audisc/ui/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/audisc/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.theleafapps.pro.audisc.ui
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.theleafapps.pro.audisc.ui.PlaylistActivity
@@ -13,6 +15,7 @@ import com.theleafapps.pro.audisc.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,11 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_Audisc)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // for Navigation Drawer
+        toggle = ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.shuffleBtn.setOnClickListener {
             val intent = Intent(this@MainActivity, PlayerActivity::class.java)
@@ -56,5 +64,11 @@ class MainActivity : AppCompatActivity() {
                 ActivityCompat.requestPermissions(this,
                     arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),10)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
     }
 }
