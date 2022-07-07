d8f5706ce7551ea8efcb759e24b883188a33604c
abhijeet <redefinedabhi2@gmail.com>
2022-07-08 01:59:36 +0530

    Getting All Audio from MediaStrore in MainActivity
    et

--- app/src/main/java/com/theleafapps/pro/audisc/ui/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/audisc/ui/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.theleafapps.pro.audisc.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -12,7 +17,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.theleafapps.pro.audisc.ui.PlaylistActivity
 import com.theleafapps.pro.audisc.R
 import com.theleafapps.pro.audisc.adapters.MusicAdapter
+import com.theleafapps.pro.audisc.data.Music
 import com.theleafapps.pro.audisc.databinding.ActivityMainBinding
+import java.io.File
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +27,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var musicAdapter: MusicAdapter
+
+    companion object{
+        lateinit var MusicListMA : ArrayList<Music>
+        lateinit var musicListSearch : ArrayList<Music>
+        var search: Boolean = false
+        var themeIndex: Int = 0
+        val currentTheme = arrayOf(R.style.coolPink, R.style.coolBlue, R.style.coolPurple, R.style.coolGreen, R.style.coolBlack)
+        val currentThemeNav = arrayOf(R.style.coolPinkNav, R.style.coolBlueNav, R.style.coolPurpleNav, R.style.coolGreenNav,
+            R.style.coolBlackNav)
+        val currentGradient = arrayOf(R.drawable.gradient_pink, R.drawable.gradient_blue, R.drawable.gradient_purple, R.drawable.gradient_green,
+            R.drawable.gradient_black)
+        var sortOrder: Int = 0
+        val sortingList = arrayOf(MediaStore.Audio.Media.DATE_ADDED + " DESC", MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.SIZE + " DESC")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,6 +92,41 @@ class MainActivity : AppCompatActivity() {
 
         musicAdapter = MusicAdapter(this,musicList)
         binding.musicRV.adapter = musicAdapter
+    }
+
+    @SuppressLint("Recycle", "Range")
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun getAllAudio(): ArrayList<Music>{
+        val tempList = ArrayList<Music>()
+        val selection = MediaStore.Audio.Media.IS_MUSIC +  " != 0"
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID, MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.DURATION, MediaStore.Audio.Media.DATE_ADDED,
+            MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.ALBUM_ID)
+        val cursor = this.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection,selection,null,
+            sortingList[sortOrder], null)
+        if(cursor != null){
+            if(cursor.moveToFirst())
+                do {
+                    val titleC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))?:"Unknown"
+                    val idC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID))?:"Unknown"
+                    val albumC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))?:"Unknown"
+                    val artistC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))?:"Unknown"
+                    val pathC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
+                    val durationC = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
+                    val albumIdC = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)).toString()
+                    val uri = Uri.parse("content://media/external/audio/albumart")
+                    val artUriC = Uri.withAppendedPath(uri, albumIdC).toString()
+                    val music = Music(id = idC, title = titleC, album = albumC, artist = artistC, path = pathC, duration = durationC,
+                        artUri = artUriC)
+                    val file = File(music.path)
+                    if(file.exists())
+                        tempList.add(music)
+                }while (cursor.moveToNext())
+            cursor.close()
+        }
+        return tempList
     }
 
     private fun requestRuntimePermission(){
